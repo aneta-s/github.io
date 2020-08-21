5376864315f66886f400a9527a064c01daff5bf1
aneta-s <54153719+aneta-s@users.noreply.github.com>
2020-08-21 12:04:44 +0200

    Updates

---
+++ precache-manifest.cd4f03ff854bc26e796904ea94113d1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433b9c49f69d249fea711019a0dcca14",
+    "url": "/samburinga/index.html"
+  },
+  {
+    "revision": "d239cbc735b40fa6dddb",
+    "url": "/samburinga/static/css/main.cf6e2fe9.chunk.css"
+  },
+  {
+    "revision": "047e707c7ced4e2624a1",
+    "url": "/samburinga/static/js/2.f0f2b6f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samburinga/static/js/2.f0f2b6f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d239cbc735b40fa6dddb",
+    "url": "/samburinga/static/js/main.5b65c0d1.chunk.js"
+  },
+  {
+    "revision": "715e3fa61144413ccf4c",
+    "url": "/samburinga/static/js/runtime-main.4a4e6c37.js"
+  }
+]);
